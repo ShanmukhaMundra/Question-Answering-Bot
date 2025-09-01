2873d50afa4c10db3a81ed0a9ca1dfee54da9fda
shanu0006 <shanumundra66@gmail.com>
2025-09-01 14:18:37 -0500

    files

---
+++ preprocessing.py
@@ -0,0 +1,16 @@
+import nltk
+import json
+print(f"Hello! I'm (the QA bot name you chose), a question answering bot who knows answers to all questions from the 'Jeopardy!' game.")
+print('Ask me something!')
+user_input = input(">")
+print("Let's play!")
+with open('data/atum.json', 'r') as f:
+    data = json.load(f)
+def tokenize(user_input):
+    text = user_input.lower()
+    replacement = ['?', '!', '.', ',', ';', '&', '*', '_', '+', '=', '`']
+    for mark in replacement:
+        text = text.replace(mark, '')
+    tokens = nltk.word_tokenize(text)
+    return tokens
+print(tokenize(user_input))
