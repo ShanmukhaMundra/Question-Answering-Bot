0267dc51541fa7a5cd2613c2138e2dfc23490225
shanu0006 <shanumundra66@gmail.com>
2025-09-01 14:02:34 -0500

    files

--- final.py
+++ final.py
@@ -1,4 +1,4 @@
 print(f"Hello! I'm Atum, a question answering bot who knows answers to all questions from the 'Jeopardy!' game.")
 print('Ask me something!')
-user_input = input(">")
+user_input = input("> ")
 print("Let's play!")
